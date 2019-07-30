cffba3f0d4487460a19ba30c2dce59853ea87ecf
Eugene Birondo <jbirondo@gmail.com>
2019-07-30 14:06:52 -0700

    User Auth continued

---
+++ frontend/components/app.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import SignupContainer from './session/signup_container';
+import LoginContainer from './session/login_container';
+import Home from './home/home';
+import { Route } from 'react-router-dom';
+import { AuthRoute, ProtectedRoute } from '../utils/route_util';
+
+export default () => (
+    <div>
+        <Route path="/" component={NavBarContainer} />
+        <Route exact path="/" component={Home} />
+        <AuthRoute path="/signup" component={SignupContainer} />
+        <AuthRoute path="/login" component={LoginContainer} />
+    </div>
+);
