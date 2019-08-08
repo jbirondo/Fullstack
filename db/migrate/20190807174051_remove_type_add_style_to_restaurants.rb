f3c1252d42a10812c5a669b63acda9d500c5db6d
Eugene Birondo <jbirondo@gmail.com>
2019-08-08 13:57:36 -0700

    Heroku issues

--- db/migrate/20190807174051_remove_type_add_style_to_restaurants.rb
+++ db/migrate/20190807174051_remove_type_add_style_to_restaurants.rb
@@ -1,6 +1,5 @@
 class RemoveTypeAddStyleToRestaurants < ActiveRecord::Migration[5.2]
   def change
-            remove_column :restaurants, :type, :string
             add_column :restaurants, :style, :string
   end
 end
