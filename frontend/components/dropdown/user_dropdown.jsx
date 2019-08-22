dc69e1772546659dc57c9d3ead83855169834588
Eugene Birondo <jbirondo@gmail.com>
2019-08-21 21:42:14 -0700

    Changes to nav bar

--- frontend/components/dropdown/user_dropdown.jsx
+++ frontend/components/dropdown/user_dropdown.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import {logout} from '../../actions/session'
-// import LoginFormContainer from '../session/login_container';
-// import SignupFormContainer from '../session/signup_container';
 
 
 class userDropdown extends React.Component {
@@ -12,47 +10,21 @@ class userDropdown extends React.Component {
         this.state = { hidden: true };
         this.addHiddenClass = this.addHiddenClass.bind(this);
     }
-    // if (!modal) {
-    // return null;
-    // }
-    
-    // switch (modal) {
-    //     let component
-    //     case 'user-nav-dropdown':
-    //     component = <LoginFormContainer />;
-    //     break;
-    //     case 'signup': //restaurant-profile
-    //     component = <SignupFormContainer />;
-    //     break;
-    //     default:
-    //     return null;
-    // }
-    
 
     addHiddenClass() {
         this.setState({hidden: !this.state.hidden});
     }
 
     render() {
-        // debugger
         return (
             <div className="user-dropdown-button">
-                <button onClick={this.addHiddenClass}><h3 className="nav-bar-welcome-message">Hi, {this.props.currentUser.email} ⋁</h3></button>
+                <button onClick={this.addHiddenClass}><h3 className="nav-bar-welcome-message">{this.props.currentUser.firstName[0]} ⋁</h3></button>
                 <ul className={this.state.hidden ? 'user-dropdown-menu hide' : 'user-dropdown-menu' }>
                     <li><button className="logout-button" onClick={this.props.logout}>Sign out</button></li>
                 </ul>
             </div>
         )    
     }
-
-    // render () {  
-    //     return (
-    //         <div >
-    //             <button onClick={this.addHiddenClass}></button>
-    //             {this.renderDropDown()}
-    //         </div>
-    //     )
-    // }
 } 
 
 const mapStateToProps = state => {
--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -16,6 +16,7 @@ export default ({ currentUser, logout, openModal }) => {
                 <Link to="/" className="header-link">
                     <h2>Open Table </h2>
                 </Link>
+                
                 <button className="nav-sign-in-button" onClick={() => openModal('login')}>Sign in</button>
                 <button className="nav-sign-up-button" onClick={() => openModal('signup')}>Sign up</button>
             </div>
