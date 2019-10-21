98d12bfbc786e2ef9eb95e932ffc2a5abc427ce6
Eugene Birondo <jbirondo@gmail.com>
2019-10-21 15:05:45 -0700

    Seperating into neighborhoods

--- frontend/components/restaurant/restaurant_index.jsx
+++ frontend/components/restaurant/restaurant_index.jsx
@@ -17,7 +17,26 @@ class RestaurantIndex extends React.Component {
       )
     } 
     const restaurantArray = Object.keys(this.props.restaurants).map( key => this.props.restaurants[key])
-    
+    const gryff = new Array
+    const huffle = new Array
+    const slyth = new Array
+    const raven = new Array
+    restaurantArray.forEach(restaurant => {
+        if (restaurant.neighborhood == "Gryffindor") {
+          gryff.push(restaurant)
+        } else if (restaurant.neighborhood == "Hufflepuff"){
+          huffle.push(restaurant)
+        } else if (restaurant.neighborhood == "Slytherin"){
+          slyth.push(restaurant)
+        } else if (restaurant.neighborhood == "Ravenclaw"){
+          raven.push(restaurant)
+        }
+      }
+    )
+    console.log(gryff)
+    console.log(huffle)
+    console.log(slyth)
+    console.log(raven)
     return(
       <ul>
         {restaurantArray.map(restaurant => 
