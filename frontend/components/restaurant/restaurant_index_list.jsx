cf94d30fc155d0814b77dd889767eef35dd3eb83
Eugene Birondo <jbirondo@gmail.com>
2019-10-22 23:40:15 -0700

    Changed CSS and added function to limit number of li's in index

--- frontend/components/restaurant/restaurant_index_list.jsx
+++ frontend/components/restaurant/restaurant_index_list.jsx
@@ -3,12 +3,29 @@ import { Link } from "react-router-dom"
 class RestaurantIndexList extends React.Component {
     constructor(props) {
         super(props)
+        this.pos1 = 0
+        this.pos2 = 4
+    }
+
+    window(arr) {
+        return arr.slice(this.pos1, this.pos2)
+    }
+
+    nextWindow(arr) {
+        return this.window(arr, this.pos1 + 4, this.pos2 + 4)
+    }
+
+    componentDidUpdate(prevProps) {
+        // if (prevProps.match.params.restaurantId !== this.props.match.params.restaurantId) {
+        //     this.props.requestRestaurant(this.props.match.params.restaurantId);
+        // }
     }
 
     render() {
+        const eles = this.window(this.props.restaurants)
         return(
             <ul className="restaurant-index-ul">
-                {this.props.restaurants.map(restaurant =>
+                {eles.map(restaurant =>
                     <li className="restaurant-index-li" key={restaurant.id}>
                         <Link className="restaurant-index-link" to={`/restaurants/${restaurant.id}`}>
                             <div>{restaurant.name}</div>
@@ -16,7 +33,11 @@ class RestaurantIndexList extends React.Component {
                             <div>{restaurant.style}</div>
                         </Link>
                     </li>)}
-                <button className="restaurant-index-ul-advancing-button">></button>
+                <button 
+                    className="restaurant-index-ul-advancing-button"
+                    onClick={() => this.nextWindow(this.props.restaurants, this.pos1, this.pos2)}
+                    >>
+                </button>
             </ul>
         )
     }
