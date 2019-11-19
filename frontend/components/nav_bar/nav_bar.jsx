a3a852e0b3ce3d71fce2eed679c5afc3ba8a8f79
Eugene Birondo <jbirondo@gmail.com>
2019-11-18 23:31:49 -0800

    Social links added

--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -1,48 +1,58 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 import UserDropdown from '../dropdown/user_dropdown'
+// import Logo from "./logo.png"
 
 export default ({ currentUser, logout, openModal }) => {
     
     
     const display = currentUser ? (
       <div className="nav-bar clearfix">
-        <Link to="/" className="header-link">
-          <h2>Open Table </h2>
-        </Link>
-        <div>
-          <div className="social-links">
-            <a href="https://github.com/jbirondo">
-              <i className="fab fa-github"></i>
-            </a>
-            <a href="https://www.linkedin.com/in/eugene-birondo-702637192/">
-              <i className="fab fa-linkedin-in"></i>
-            </a>
-            <a href="https://angel.co/eugene-birondo">
-              <i className="fab fa-angellist"></i>
-            </a>
+        <div className="left-nav-bar">
+          <Link to="/" className="header-link">
+            <h2>Open Table </h2>
+          </Link>
+            <img 
+                src={require('./logo.png')} 
+                alt=" "
+                className="logo"
+            />
+          <div>
+            <div className="social-links">
+              <a href="https://github.com/jbirondo">
+                <i className="fab fa-github fa-2x"></i>
+              </a>
+              <a href="https://www.linkedin.com/in/eugene-birondo-702637192/">
+                <i className="fab fa-linkedin-in fa-2x"></i>
+              </a>
+              <a href="https://angel.co/eugene-birondo">
+                <i className="fab fa-angellist fa-2x"></i>
+              </a>
+            </div>
           </div>
         </div>
         <div>
-            <UserDropdown />
+          <UserDropdown />
         </div>
         {/* needs a link to reservations */}
       </div>
     ) : (
       <div className="nav-bar">
-        <Link to="/" className="header-link">
-          <h2>Open Table </h2>
-        </Link>
-        <div className="social-links">
-          <a href="https://github.com/jbirondo">
-            <i className="fab fa-github"></i>
-          </a>
-          <a href="https://www.linkedin.com/in/eugene-birondo-702637192/">
-            <i className="fab fa-linkedin-in"></i>
-          </a>
-          <a href="https://angel.co/eugene-birondo">
-            <i className="fab fa-angellist"></i>
-          </a>
+        <div className="left-nav-bar">
+          <Link to="/" className="header-link">
+            <h2>Open Table </h2>
+          </Link>
+          <div className="social-links">
+            <a href="https://github.com/jbirondo">
+              <i className="fab fa-github fa-2x"></i>
+            </a>
+            <a href="https://www.linkedin.com/in/eugene-birondo-702637192/">
+              <i className="fab fa-linkedin-in fa-2x"></i>
+            </a>
+            <a href="https://angel.co/eugene-birondo">
+              <i className="fab fa-angellist fa-2x"></i>
+            </a>
+          </div>
         </div>
         <div>
           <button
