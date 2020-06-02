60212c5e96f1c2ab9be5f3cf19963791abe34a2a
Eugene Birondo <jbirondo@gmail.com>
2020-06-02 16:53:07 -0700

    Changed session_form from div to form and added onSubmit class to form

--- frontend/components/session/session_form.jsx
+++ frontend/components/session/session_form.jsx
@@ -16,7 +16,7 @@ class SessionForm extends React.Component {
     handleSubmit(e) {
         e.preventDefault();
         const user = Object.assign({}, this.state);
-        debugger
+        // debugger
         this.props.processForm(user).then(this.props.closeModal);
     }
 
