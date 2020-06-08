518039511f7f8c22d7a05ef83cbd737b6934d658
Eugene Birondo <jbirondo@gmail.com>
2020-06-08 11:03:53 -0700

    Deleting debuggers/console.log

--- frontend/components/reservations/reservation_create_container.jsx
+++ frontend/components/reservations/reservation_create_container.jsx
@@ -9,7 +9,6 @@ const mapStateToProps = state => {
 }
 
 const mapDispatchToProps = dispatch => {
-    // debugger
     return {
         createReservation: (reservation) => dispatch(postReservation(reservation))
     }
